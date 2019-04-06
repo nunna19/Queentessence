93bafd3a190983c9b2798ad86f7f1a43d54490be
nunna19 <nunnapat_ch@yahoo.com>
2019-04-06 19:03:31 -0400

    Updates

---
+++ precache-manifest.e3f55961a53d087f7cf1b8dc682ed27a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94435833360241e23baf",
+    "url": "/Queentessence/static/css/main.a6aaafb7.chunk.css"
+  },
+  {
+    "revision": "94435833360241e23baf",
+    "url": "/Queentessence/static/js/main.1fc1705f.chunk.js"
+  },
+  {
+    "revision": "606103c84c8a50ec490a",
+    "url": "/Queentessence/static/js/runtime~main.273d09ca.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Queentessence/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Queentessence/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5b6ec4009e189e9cd439479c5223c01",
+    "url": "/Queentessence/index.html"
+  }
+];
