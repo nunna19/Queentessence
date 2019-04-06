841d9469eaa1e40fdad0685da1aba70107e69ef8
nunna19 <nunnapat_ch@yahoo.com>
2019-04-06 19:01:52 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,87 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/style.scss';
 
 class App extends Component {
+
+  state ={
+    flip:false
+  }
+
+flip = () => { 
+  this.setState({
+    flip : !this.state.flip
+  })
+}
+
+toFront = () => {
+  this.setState({flip:false})
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="grid-container">
+          <div className="grid-Header">
+
+          </div>
+
+          <div className="grid-Main">
+            <div className={this.state.flip ? "flip-container front" : "flip-container"} onClick={this.flip} >
+              <div className="flipper">
+                <div className= "card tile-front">
+                  <div className="bg-tile">
+                    <i className="material-icons m_emote">account_box</i>
+                  </div>
+                  <div className="card-body">
+                    <p>5 Users</p>
+                  </div>
+                  <div className="card-footer text-muted m_desc">
+                      Online
+                  </div>
+                </div>
+
+                  <div className="card tile-back" onMouseLeave={this.toFront}>
+                    <div class="card-body">
+                      <p>User online correspond to people connected on your network</p>
+                    </div>
+                  </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className="grid-Header">
+              Header
+          </div>
+
+         <div className="grid-Right">
+              right
+          </div>
+
+          <div className="grid-Menu">
+              menu
+          </div>
+
+          <div className="grid-Footer">
+              footer
+          </div>
+
+          <div className="colum1">
+              colum1
+              </div>
+              <div className="colum2">
+                colum2
+              </div>
+              <div className="colum3">
+                colum3
+              </div>
+       </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
+// {this.state.flip ? "flip-container front" : "flip-container"} 
